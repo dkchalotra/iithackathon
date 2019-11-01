5bc28584bdc681d2ebf1f979a4fcb243e8b0e982
Dheeraj Kumar <cod3rboy@hotmail.com>
2019-11-02 00:02:35 +0530

    Added base layout file

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+<html lang="en">
+    <head>
+        <title>@yield('page_title')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        @section('head_includes')
+            <link rel="stylesheet" href="css/bootstrap">
+        @show
+    </head>
+    <body>
+        @section('content')
+        @show
+        @section('foot_includes')
+        <script src="js/jquery-3.4.1.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.js"></script>
+        @show
+    </body>
+</html>
