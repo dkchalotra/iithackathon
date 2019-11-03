ed3e2d0ae7591089168c5dbbee387245dbfba3d4
Hilal Ahmad <hilalahmad456@gmail.com>
2019-11-03 14:45:38 +0530

    search box added

--- resources/views/layouts/design.blade.php
+++ resources/views/layouts/design.blade.php
@@ -9,23 +9,21 @@
             <link rel="stylesheet" href="css/fontawesome.css">
             <link rel="stylesheet" href="css/default.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
         @show
     </head>
     <body>
         <div class="row" style="height: 18vh">
             <div class="col-2">
-                <img src="images\iitjammu.jpg" alt="logo" style="height: 7pc;width: 8pc;border-radius: 50%;margin: 3% 3% 0% 3%">
+                <img src="images\iitjammu.jpg" alt="logo" style="height: 7pc;width: 8pc;border-radius: 50%;margin: 4% 3% 0% 3%">
             </div>
 
             <div class="col-8">
                 <h3 class="text-center" style="margin-top: 2%">Indian Institute Of Technology</h3>
-                <p class="text-center">Jagti, Jammu, J&amp;K</p>
-
+                <p class="text-center">Jammu,Jakti(Jammu Kashmir)</p>
             </div>
 
-            <div class="col-2">
-                <div class="social_media" style="margin-top:10%;margin-left:2%;font-size: 23pt">
+            <div class="col-2">                
+                <div class="social_media" style="margin-top:10%;margin-left:2%;font-size: 23pt">                    
                     <a href="https://facebook.com" target="_blank" class="icoFacebook" title="Facebook"><i class="fab fa-facebook"></i></a>
                     <a href="https://twitter.com" target="_blank" class="icoTwitter" title="Twitter"> <i class="fab fa-twitter"></i></a>
                     <a href="https://www.google.com" target="_blank" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a>
@@ -33,12 +31,11 @@
                     <a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a>
                 </div>
             </div>
-
         </div>
-
+       
         <div class="text-center" style="background-color:#bfbfbf;color:#000000;font-size:16pt;font-weight:100;letter-spacing:0.2em;height:5vh;">
             @yield('title', 'Admin Panel')
-            <a class="btn btn-sm btn-secondary" style="float:right;letter-spacing:0;" href="/logout">Logout</a>
+            <a class="btn btn-sm btn-secondary" style="float:right;letter-spacing:0; margin-right:5px; margin-top:4px;" href="/logout">Logout</a>
         </div>
         <div class="container-fluid" style="padding:0;">
             <div class="row">
